cc3476a466f49eb098855c428b0c137a60e294ea
Gabriel Carmona <gabriel.carmonat@sansano.usm.cl>
2024-03-11 15:21:23 +0100

    add grouping matrices

--- write_matrix.py
+++ write_matrix.py
@@ -384,4 +384,203 @@ def mixtral_divided_matrices(model, name, quant):
         write_matrices(dir_file + name_file + "-qzeros", model.model.layers[i].self_attn.v_proj.qzeros.numpy())
         write_matrices(dir_file + name_file + "-scales", model.model.layers[i].self_attn.v_proj.scales.numpy())
 
+def llama_matrices_stacked(model, name, quant, group):
+    files = []
+    shapes = []
+    dir_file = save_dir + name + "/" + quant + "/stacked/"
+    down_proj = None
+    gate_proj = None
+    up_proj = None
+    k_proj = None
+    o_proj = None
+    q_proj = None
+    v_proj = None
+    count = 0
+    for i in range(len(model.model.layers)):
+        count += 1
+        qweight = model.model.layers[i].mlp.down_proj.qweight
+        qzeros = model.model.layers[i].mlp.down_proj.qzeros
+        scales = model.model.layers[i].mlp.down_proj.scales
+        bits = model.model.layers[i].mlp.down_proj.bits
+        wf = model.model.layers[i].mlp.down_proj.wf
+        group_size = model.model.layers[i].mlp.down_proj.group_size
 
+        if down_proj is None:
+            down_proj = final_matrix(qzeros, scales, qweight, bits, group_size, wf).numpy()
+        else:
+            down_proj = np.concatenate((down_proj, final_matrix(qzeros, scales, qweight, bits,
+                group_size, wf).numpy()))
+    
+        qweight = model.model.layers[i].mlp.gate_proj.qweight
+        qzeros = model.model.layers[i].mlp.gate_proj.qzeros
+        scales = model.model.layers[i].mlp.gate_proj.scales
+        bits = model.model.layers[i].mlp.gate_proj.bits
+        wf = model.model.layers[i].mlp.gate_proj.wf
+        group_size = model.model.layers[i].mlp.gate_proj.group_size
+
+        if gate_proj is None:
+            gate_proj = final_matrix(qzeros, scales, qweight, bits, group_size, wf).numpy()
+        else:
+            gate_proj = np.concatenate((gate_proj, final_matrix(qzeros, scales, qweight, bits,
+                group_size, wf).numpy()))
+
+        qweight = model.model.layers[i].mlp.up_proj.qweight
+        qzeros = model.model.layers[i].mlp.up_proj.qzeros
+        scales = model.model.layers[i].mlp.up_proj.scales
+        bits = model.model.layers[i].mlp.up_proj.bits
+        wf = model.model.layers[i].mlp.up_proj.wf
+        group_size = model.model.layers[i].mlp.up_proj.group_size
+
+        if up_proj is None:
+            up_proj = final_matrix(qzeros, scales, qweight, bits, group_size, wf).numpy()
+        else:
+            up_proj = np.concatenate((up_proj, final_matrix(qzeros, scales, qweight, bits,
+                group_size, wf).numpy()))
+
+        qweight = model.model.layers[i].self_attn.k_proj.qweight
+        qzeros = model.model.layers[i].self_attn.k_proj.qzeros
+        scales = model.model.layers[i].self_attn.k_proj.scales
+        bits = model.model.layers[i].self_attn.k_proj.bits
+        wf = model.model.layers[i].self_attn.k_proj.wf
+        group_size = model.model.layers[i].self_attn.k_proj.group_size
+
+        if k_proj is None:
+            k_proj = final_matrix(qzeros, scales, qweight, bits, group_size, wf).numpy()
+        else:
+            k_proj = np.concatenate((k_proj, final_matrix(qzeros, scales, qweight, bits,
+                group_size, wf).numpy()))
+
+        qweight = model.model.layers[i].self_attn.o_proj.qweight
+        qzeros = model.model.layers[i].self_attn.o_proj.qzeros
+        scales = model.model.layers[i].self_attn.o_proj.scales
+        bits = model.model.layers[i].self_attn.o_proj.bits
+        wf = model.model.layers[i].self_attn.o_proj.wf
+        group_size = model.model.layers[i].self_attn.o_proj.group_size
+
+        if o_proj is None:
+            o_proj = final_matrix(qzeros, scales, qweight, bits, group_size, wf).numpy()
+        else:
+            o_proj = np.concatenate((o_proj, final_matrix(qzeros, scales, qweight, bits,
+                group_size, wf).numpy()))
+
+        qweight = model.model.layers[i].self_attn.q_proj.qweight
+        qzeros = model.model.layers[i].self_attn.q_proj.qzeros
+        scales = model.model.layers[i].self_attn.q_proj.scales
+        bits = model.model.layers[i].self_attn.q_proj.bits
+        wf = model.model.layers[i].self_attn.q_proj.wf
+        group_size = model.model.layers[i].self_attn.q_proj.group_size
+
+        if q_proj is None:
+            q_proj = final_matrix(qzeros, scales, qweight, bits, group_size, wf).numpy()
+        else:
+            q_proj = np.concatenate((q_proj, final_matrix(qzeros, scales, qweight, bits,
+                group_size, wf).numpy()))
+
+        qweight = model.model.layers[i].self_attn.v_proj.qweight
+        qzeros = model.model.layers[i].self_attn.v_proj.qzeros
+        scales = model.model.layers[i].self_attn.v_proj.scales
+        bits = model.model.layers[i].self_attn.v_proj.bits
+        wf = model.model.layers[i].self_attn.v_proj.wf
+        group_size = model.model.layers[i].self_attn.v_proj.group_size
+
+        if v_proj is None:
+            v_proj = final_matrix(qzeros, scales, qweight, bits, group_size, wf).numpy()
+        else:
+            v_proj = np.concatenate((v_proj, final_matrix(qzeros, scales, qweight, bits,
+                group_size, wf).numpy()))
+
+        if count == group:
+            name_file = "mlp-down_proj-" + str(i)
+            shapes.append("x".join([str(down_proj.shape[0]), str(down_proj.shape[1])]))
+            files.append(name_file)
+            
+            write_matrices(dir_file + name_file, down_proj)
+
+            name_file = "mlp-gate_proj-" + str(i)
+            shapes.append("x".join([str(gate_proj.shape[0]), str(gate_proj.shape[1])]))
+            files.append(name_file)
+            
+            write_matrices(dir_file + name_file, gate_proj)
+
+            name_file = "mlp-up_proj-" + str(i)
+            shapes.append("x".join([str(up_proj.shape[0]), str(up_proj.shape[1])]))
+            files.append(name_file)
+            
+            write_matrices(dir_file + name_file, up_proj)
+
+            name_file = "self_attn-k_proj-" + str(i)
+            shapes.append("x".join([str(k_proj.shape[0]), str(k_proj.shape[1])]))
+            files.append(name_file)
+            
+            write_matrices(dir_file + name_file, k_proj)
+
+            name_file = "self_attn-q_proj-" + str(i)
+            shapes.append("x".join([str(q_proj.shape[0]), str(q_proj.shape[1])]))
+            files.append(name_file)
+            
+            write_matrices(dir_file + name_file, q_proj)
+
+            name_file = "self_attn-o_proj-" + str(i)
+            shapes.append("x".join([str(o_proj.shape[0]), str(o_proj.shape[1])]))
+            files.append(name_file)
+            
+            write_matrices(dir_file + name_file, o_proj)
+
+            name_file = "self_attn-v_proj-" + str(i)
+            shapes.append("x".join([str(v_proj.shape[0]), str(v_proj.shape[1])]))
+            files.append(name_file)
+            
+            write_matrices(dir_file + name_file, v_proj)
+            
+            down_proj = None
+            gate_proj = None
+            up_proj = None
+            k_proj = None
+            o_proj = None
+            q_proj = None
+            v_proj = None
+            count = 0
+    if count != 0:
+        name_file = "mlp-down_proj-" + str(i)
+        shapes.append("x".join([str(down_proj.shape[0]), str(down_proj.shape[1])]))
+        files.append(name_file)
+        
+        write_matrices(dir_file + name_file, down_proj)
+
+        name_file = "mlp-gate_proj-" + str(i)
+        shapes.append("x".join([str(gate_proj.shape[0]), str(gate_proj.shape[1])]))
+        files.append(name_file)
+        
+        write_matrices(dir_file + name_file, gate_proj)
+
+        name_file = "mlp-up_proj-" + str(i)
+        shapes.append("x".join([str(up_proj.shape[0]), str(up_proj.shape[1])]))
+        files.append(name_file)
+        
+        write_matrices(dir_file + name_file, up_proj)
+
+        name_file = "self_attn-k_proj-" + str(i)
+        shapes.append("x".join([str(k_proj.shape[0]), str(k_proj.shape[1])]))
+        files.append(name_file)
+        
+        write_matrices(dir_file + name_file, k_proj)
+
+        name_file = "self_attn-q_proj-" + str(i)
+        shapes.append("x".join([str(q_proj.shape[0]), str(q_proj.shape[1])]))
+        files.append(name_file)
+        
+        write_matrices(dir_file + name_file, q_proj)
+
+        name_file = "self_attn-o_proj-" + str(i)
+        shapes.append("x".join([str(o_proj.shape[0]), str(o_proj.shape[1])]))
+        files.append(name_file)
+        
+        write_matrices(dir_file + name_file, o_proj)
+
+        name_file = "self_attn-v_proj-" + str(i)
+        shapes.append("x".join([str(v_proj.shape[0]), str(v_proj.shape[1])]))
+        files.append(name_file)
+        
+        write_matrices(dir_file + name_file, v_proj)
+
+    print("command: ", "--files " + ":".join(files), "--sizes " + ":".join(shapes))
